3ac5348fbf05c26489960881f5c3059257aaf65a
durgaprasad.jarapala <durgaprasad.jarapala@kotak.com>
2025-11-01 08:27:00 +0530

    body styling fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import {useState, useEffect} from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Header from "./components/header/Header";
@@ -7,14 +7,21 @@ import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
 import ErrorPage from "./components/errorPage/ErrorPage";
 import RestaurantMenu from "./components/restaurantMenu/RestaurantMenu";
-import "./index.css";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 const AppLayout = () => {
+  const [headerHeight, setHeaderHeight] = useState(0);
+  useEffect(() => {
+    const header = document.querySelector(".header");
+    if (header) {
+      setHeaderHeight(header.offsetHeight);
+    }
+  }, []);
+
   return (
     <div className="app">
       <Header />
-      <div style={{paddingTop:100}}>
+      <div style={{ paddingTop: `${headerHeight}px` }}>
         <Outlet />
       </div>
     </div>
